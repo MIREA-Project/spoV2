3ddca0f55a5762dedbd7076fc042b1a690819e98
danek52 <guskir3565@gmail.com>
2025-02-28 10:23:42 +0300

    add more context to graphql

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import logging
 from contextlib import asynccontextmanager
-from db import get_session
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from strawberry.fastapi import BaseContext
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 
@@ -12,6 +8,7 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from db.models import *
 from db import init_models
+from modules.graphql.context import get_context
 from modules.graphql.example import Query
 from redis_initializer import init_redis, close_redis
 from modules.reg_module.routes import router
@@ -36,18 +33,8 @@ app = FastAPI(
     }
 )
 app.include_router(router)
-# Монтируем GraphQL роутер в FastAPI
-# Создаем схему GraphQL
-# Контекст для GraphQL
-class GraphqlContext(BaseContext):
-    def __init__(self, db_session: AsyncSession):
-        self.db_session = db_session
 
-
-# Функция для создания контекста
-async def get_context(db_session: AsyncSession = Depends(get_session)) -> GraphqlContext:
-    return GraphqlContext(db_session=db_session)
-
+# add graphql router
 schema = strawberry.Schema(query=Query)
 graphql_app = GraphQLRouter(schema, context_getter=get_context)
 app.include_router(graphql_app, prefix="/graphql")
